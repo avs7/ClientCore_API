d4f8f7f54d19ca543adcde97a74cfcb4c6b4699f
Aleksandr <aslyusarev@gmail.com>
2023-12-06 11:51:07 -0800

    finished server set-up

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
+const contactRoutes = require('./routes/contacts')
+const userRoutes = require('./routes/user')
 
 const app = express()
+
+app.use(express.json())
+app.use('/api/contacts', contactRoutes)
+app.use('/api/user', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('test123')
 })
 
-app.listen(3000, () => {
-  console.log('listening on port 3000')
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to DB & listening on port ${process.env.PORT}`)
+    })
+  })
+  .catch(err => console.log(err))
