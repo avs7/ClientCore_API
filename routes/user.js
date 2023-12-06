ded2abf1d28db31dc4aa38b2cebb9392cd966220
Aleksandr <aslyusarev@gmail.com>
2023-12-06 11:17:27 -0800

    set up auth middleware

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,6 @@ const { signupUser, loginUser } = require('../controllers/userController')
 const router = express.Router()
 
 router.post('/login', loginUser)
-
 router.post('/signup', signupUser)
 
 module.exports = router
